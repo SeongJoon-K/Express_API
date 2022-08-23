c1a07c4696b9be0c131367d174e751074aa7560f
SEONGJOON KIM <jobcho6320@naver.com>
2022-08-23 23:38:17 +0900

    8/23 express routing

---
+++ serverWithExpress.js
@@ -0,0 +1,25 @@
+const modularizedFunctions = require('./modularizedFunctions.js')
+const express = require('express');
+const app = express()
+
+app.use(express.json())
+
+//get 요청 처리 라우팅
+app.get('/ping', (req, res) => {res.json({ message: '/pong'})})
+app.get('/users', modularizedFunctions.getUsers)
+app.get('/users/:userId', modularizedFunctions.getUserByUserId)
+app.get('/posts', modularizedFunctions.getPosts)
+
+//post 요청 처리 라우팅
+app.post('/users', modularizedFunctions.createUser)
+app.post('/posts', modularizedFunctions.createPost)
+
+//patch 요청 처리 라우팅
+app.patch('/posts/:postId', modularizedFunctions.updatePost)
+
+//delete 요청 처리 라우팅
+app.delete('/posts/:postId', modularizedFunctions.deletePost)
+
+app.listen(3000, "127.0.0.1", function() {
+  console.log('listening on port 3000')
+})
